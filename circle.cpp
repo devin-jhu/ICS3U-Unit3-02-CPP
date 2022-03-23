56ec317bc89265c1918e088e11862f0d97e8dea6
Devin Jhu <devin.jhu@mths.ca>
2022-03-23 04:08:53 +0000

    code

--- circle.cpp
+++ circle.cpp
@@ -2,24 +2,29 @@
 //
 // Created by Devin Jhu
 // Created on March 2022
-// The proper format "Hello, World!"
+// The number game
 
 #include <iostream>
 
 int main() {
-    // this function calculates circumference
-    const double TAU = 6.28;
-    int radius;
-    double circumference;
+    // this function is a guessing game
+    int NumberAnswer = 7;
+    int Guess;
 
+    std::cout << "The number game";
+    std::cout << "" << std::endl;
+    std::cout << "Guess a number between 1 and 9";
     // input
-    std::cout << "enter radius (mm): ";
-    std::cin >> radius;
+    std::cout << "" << std::endl;
+    std::cout << "enter number: ";
+    std::cin >> Guess;
 
     // process
-    circumference = TAU*radius;
-
-    // output
+    if (Guess == NumberAnswer) {
+        std::cout << "You win!";
+    } else {
+        std::cout << "You lose :(";
+    }
     std::cout << "" << std::endl;
-    std::cout << "circumference is " << circumference << "mm" << std::endl;
+    std::cout << "Done.";
 }
